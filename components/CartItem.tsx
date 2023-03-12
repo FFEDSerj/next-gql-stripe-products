e918793008350afadf1c3d52fc9caff6d73b5fcf
FFEDSerj <ffedchik@gmail.com>
2023-03-12 17:24:34 +0200

    Update cart items quantity

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,11 +1,27 @@
 import Image from "next/image";
-import { CartItem } from "../types";
+import {
+  CartItem,
+  GetCartDocument,
+  useDecreaseCartItemMutation,
+  useIncreaseCartItemMutation,
+} from "../types";
+import { MinusIcon } from "./MinusIcon";
+import { PlusIcon } from "./PlusIcon";
 
 type CartItemProps = {
   item: CartItem;
+  cartId: string;
 };
 
-export function CartItem({ item }: CartItemProps) {
+export function CartItem({ item, cartId }: CartItemProps) {
+  const [increaseCartItem, { loading: increasingCartItem }] =
+    useIncreaseCartItemMutation({
+      refetchQueries: [GetCartDocument],
+    });
+  const [decreaseCartItem, { loading: decreasingCartItem }] =
+    useDecreaseCartItemMutation({
+      refetchQueries: [GetCartDocument],
+    });
   return (
     <div className="space-y-2">
       <div className="flex gap-4">
@@ -26,6 +42,28 @@ export function CartItem({ item }: CartItemProps) {
           <div className="px-2 py-1 font-light border border-neutral-700 flex-1">
             {item.quantity}
           </div>
+          <button
+            onClick={() =>
+              decreaseCartItem({
+                variables: { input: { id: item.id, cartId } },
+              })
+            }
+            disabled={decreasingCartItem}
+            className="p-1 font-light border border-neutral-700  hover:bg-black hover:text-white"
+          >
+            <MinusIcon />
+          </button>
+          <button
+            onClick={() =>
+              increaseCartItem({
+                variables: { input: { id: item.id, cartId } },
+              })
+            }
+            disabled={increasingCartItem}
+            className="p-1 font-light border border-neutral-700  hover:bg-black hover:text-white"
+          >
+            <PlusIcon />
+          </button>
         </div>
       </div>
     </div>
